ffdd2747b77c07f0e2a7788169d125f88a9fffbf
joosepalasoo <144770299+joosepalasoo@users.noreply.github.com>
2024-03-05 15:45:46 +0200

    harjutused
    harjutus1 tehtud
    harjutus2 pooleli

---
+++ harjutus2.gd
@@ -0,0 +1,18 @@
+#joosep alasoo
+#05.03.24
+#harjutus 2
+extends Node
+
+func _ready():
+	var rahakogus = 100
+	var tootesumma=randi()%200
+	if rahakogus >= tootesumma:
+		print("saad toodet osta juhuuu!")
+	elif tootesumma >= rahakogus:
+		var jaak=tootesumma-rahakogus
+		print("ei saa toodet osta puudujääev raha:",jaak)
+	print(rahakogus)
+	print(tootesumma)
+	pass
+
+	
